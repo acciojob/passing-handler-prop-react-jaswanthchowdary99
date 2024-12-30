fa251f51765d261f5e184a5a83f7407f0a3194c7
jaswanth chowdary <jassumeka@gmail.com>
2024-12-30 14:15:19 +0530

    changes made

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,23 @@
+import React, { useState } from 'react';
 
+const Selection = ({ selectedColor }) => {
+  const [boxColor, setBoxColor] = useState(""); 
+
+  const handleBoxClick = () => {
+    if (selectedColor) {
+      setBoxColor(selectedColor); 
+    }
+  };
+
+  return (
+    <div
+      className="fix-box"
+      style={{ backgroundColor: boxColor,textAlign:"center",color:'white',padding:"10px" }} 
+      onClick={handleBoxClick} 
+    >
+        Selection
+    </div>
+  );
+};
+
+export default Selection;
